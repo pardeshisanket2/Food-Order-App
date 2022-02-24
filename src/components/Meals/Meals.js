ff3f873d937720e65126051852411feab768fd43
Sanket Pardeshi <pardeshisanket2@gmail.com>
2022-02-24 17:16:18 +0530

    first commit

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import MealsSummary from './MealsSummary'
+import AvailableMeals from './AvailableMeals'
+import { Fragment } from 'react'
+
+const Meals = () => {
+  return <Fragment>
+      <MealsSummary/>
+      <AvailableMeals/>
+  </Fragment>
+}
+
+export default Meals
